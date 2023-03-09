8a0e918d89de28182fad74e7d318a08929ce2410
Mrinal Choudhary <41827353+mrinal07@users.noreply.github.com>
2023-03-09 12:17:17 +0530

    Added MovieManagementSystem(MVC) Folder

---
+++ MovieManagementSystem/MovieManagementSystem/Models/HomePageModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace MovieManagementSystem.Models
+{
+    public class HomePageModel
+    {
+        public int ProductId { get; set; }
+        [DisplayName("Product Name")]
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Count { get; set; }
+    }
+}
